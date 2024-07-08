c6467a0c92096174e6c990b062275bb744f1555c
AdarshBajpai67 <adarshbajpai67@gmail.com>
2024-07-08 22:48:40 +0530

    added recovery of message send to a disconnected client, now message can be recovered and sent in the order they were sent

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const express=require('express');
 const {createServer}=require('http');
 const path=require('path');
 const {Server}=require('socket.io');
+const sqlite3=require('sqlite3');
+const {open}=require('sqlite');
 
 const connectToDB=require('./src/config/db');
 const connectToCloudinary=require('./src/config/cloudinary');
@@ -11,42 +13,91 @@ const connectToCloudinary=require('./src/config/cloudinary');
 const authRoutes=require('./src/routes/authRoutes');
 const groupRoutes=require('./src/routes/groupRoutes');
 const userRoutes=require('./src/routes/userRoutes');
+const { access } = require('fs');
 
-const app=express();
-const server=createServer(app);
-const io=new Server(server);
+async function main(){
+  const db=await open({
+    filename: 'group_chat.db',
+    driver: sqlite3.Database
+  });
+  console.log('SQLITE Database connected');
 
-app.use(express.json());
+  await db.exec(`
+    CREATE TABLE IF NOT EXISTS messages(
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      client_offset TEXT UNIQUE,
+      message TEXT NOT NULL
+    );
+  `); 
 
-connectToDB();
-connectToCloudinary();
-
-app.use('/auth',authRoutes);
-app.use('/group',groupRoutes);
-app.use('/user',userRoutes);
-
-app.get('/check',(req,res)=>{
-    res.send('<h1>server is up and running</h1>');
-})
-
-app.get('/',(req,res)=>{
-    res.sendFile(path.join(__dirname,'index.html'))
-})
-
-io.on('connection',async (socket)=>{
-    console.log(`a user connected by id: ${socket.id}`);
-    socket.on('chat message',(msg)=>{
+  const app=express();
+  const server=createServer(app);
+  const io=new Server(server,{
+      cors:{
+          origin:'*',
+      },
+      connectionStateRecovery:true
+  });
+  
+  app.use(express.json());
+  
+  connectToDB();
+  connectToCloudinary();
+  
+  app.use('/auth',authRoutes);
+  app.use('/group',groupRoutes);
+  app.use('/user',userRoutes);
+  
+  app.get('/check',(req,res)=>{
+      res.send('<h1>server is up and running</h1>');
+  })
+  
+  app.get('/',(req,res)=>{
+      res.sendFile(path.join(__dirname,'index.html'))
+  })
+  
+  io.on('connection',async (socket)=>{
+      console.log(`a user connected by id: ${socket.id}`);
+      socket.on('chat message',async (msg,clientOffset,callback)=>{
+        let result;
+        try{
+          result=await db.run('INSERT INTO messages(message,client_offset) VALUES(?, ?)',msg,clientOffset);
+        }catch(err){
+          console.log(err);
+          if(err.errno===19){
+            callback('Duplicate message');
+          }else{}
+          return;
+        }
         console.log('message: '+msg);
-        io.emit('chat message',msg);
-    })
-    socket.on('disconnect',()=>{
-        console.log('user disconnected');
-    })
+        io.emit('chat message',msg,result.lastID);
+        callback();
+      })
 
-})
+      if(!socket.recovered){
+        try{
+          await db.each('SELECT id,message FROM messages WHERE id>?',[socket.handshake.auth.serverOffset],(_err,row)=>{
+            socket.emit('chat message',row.message,row.id);
+          });
+        }catch(err){
+          console.log(err);
+        }
+      }
 
-const PORT=process.env.PORT || 8000;
+      socket.on('disconnect',()=>{
+          console.log('user disconnected');
+      })
+  
+  })
+  
+  const PORT=process.env.PORT || 8000;
+  
+  server.listen(PORT,()=>{
+      console.log(`Server started and running on port ${PORT}`);
+  })
 
-server.listen(PORT,()=>{
-    console.log(`Server started and running on port ${PORT}`);
+}
+
+main().catch(err=>{
+  console.log(err);
 })
--- src/config/db.js
+++ src/config/db.js
@@ -5,9 +5,9 @@ const mongoose = require('mongoose')
 const connectToDB=async()=>{
     try{
         await mongoose.connect(process.env.MONGO_URL);
-        console.log('Connected to DB');
+        console.log('Connected to MongoDB');
     }catch(err){
-        console.log('Error connecting to DB',err);
+        console.log('Error connecting to MongoDB',err);
     }
 }
 
